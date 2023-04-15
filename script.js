d2372e441cc8b0c5ca6056273be0683273b3499e
Caique Costa <ccostafrias@gmail.com>
2023-04-15 14:42:08 -0300

    fix old script and some css features

--- script.js
+++ script.js
@@ -1,64 +1,103 @@
 const board = document.getElementById("board");
-const resetButton = document.getElementById("reset-button");
+const resetButton = document.querySelector(".reset-button");
 
 const sizeBoard = 10
+const widthBoard = 10
+const heightBoard = 10
 const numMines = 10
-let revealed = 0
 
+let revealed = 0
 let firstClick = true
+let isLocked = false
+
+function createTable(w, h, b = []) {
+    b = Array.from({length: h})
+        .map(r => Array.from({length: w})
+            .map(c => 0))
+
+    return b
+}
+
+function setTable(table) {
+    return table.map((cards, r) => {
+        return cards.map((card, c) => {
+            return (
+                `<div class="cell" data-row='${r}' data-column='${c}'>
+                    <div class="cell-front"></div>
+                    <div class="cell-back"></div>
+                </div>`
+            )
+        }).join('')
+    }).join('')
+}
+
+// Cria a array onde haverá todas as informações
+const boardArray = createTable(widthBoard, heightBoard)
 
 // Cria as células
-for (let i = 0; i < sizeBoard ** 2; i++) {
-    const cell = document.createElement("div");
-    cell.classList.add("cell");
-    cell.dataset.cellId = i
-    board.appendChild(cell);
-}
+board.innerHTML = setTable(boardArray)
 
-//Ajeita a grid para que fique do tamanho certo
-board.style.gridTemplateColumns = `repeat(${sizeBoard}, 1fr)`
-board.style.gridTemplateRows = `repeat(${sizeBoard}, 1fr)`
+// Ajeita a grid para que fique do tamanho certo
+board.style.gridTemplateColumns = `repeat(${widthBoard}, 1fr)`
+board.style.gridTemplateRows = `repeat(${heightBoard}, 1fr)`
 
-//Cria a array onde haverá todas as informações
-const boardArray = new Array()
-for (let row = 0; row < sizeBoard; row++) {
-    boardArray.push([])
-    for (let columns = 0; columns < sizeBoard; columns++) {
-        boardArray[row].push(0)
-    }
-}
 
-//Faz com que cada célula tenho um evento de click (esquerdo e direito)
-const cells = document.querySelectorAll('.cell')
+// Faz com que cada célula tenho um evento de click (esquerdo e direito)
+const cells = [...document.querySelectorAll('.cell')]
 cells.forEach(cell => {
-    cell.addEventListener('click', function () {
-        if (firstClick) {
-            setMines(numMines, cell.dataset.cellId)
-            firstClick = false
-        }
-        revealCell(cell.dataset.cellId)
+    cell.addEventListener('click', () => {
+        if (isLocked) return
+
+        const [row, column] = Object.values(cell.dataset).map(value => Number(value))
+
+        if (firstClick) isFirstClick(row, column)
+        else revealCell(row, column)
     })
     cell.addEventListener('contextmenu', e => {
         e.preventDefault()
-        if (cell.classList.contains('flag')) {
-            cell.classList.remove('flag')
-        } else {
-            cell.classList.add('flag')
+
+        if (isLocked) return
+
+        if (!cell.classList.contains('reveal')) {
+            if (cell.classList.contains('flag')) {
+                cell.classList.remove('flag')
+            } else {
+                cell.classList.add('flag')
+            }
         }
     })
 });
 
+board.addEventListener('contextmenu', e => {
+    e.preventDefault()
+})
+
+async function isFirstClick(r, c) {
+    firstClick = false
+
+    await setMines(numMines, r, c)
+    revealCell(r, c)
+
+}
+
 //Cria as minas em lugares aleatórios
-function setMines(mines, safeSpot) {
-    for (let m = 0; m < mines; m++) {
-        let bombR = random(sizeBoard)
-        let bombC = random(sizeBoard)
-        if (boardArray[bombR][bombC] !== 'B' && bombR * sizeBoard + bombC != safeSpot) {
+function setMines(mines, safeR = 0, safeC = 0) {
+    while (mines > widthBoard * heightBoard - 1) {
+        mines--
+    }
+
+    while (mines > 0) {
+        let bombR = random(heightBoard - 1)
+        let bombC = random(widthBoard - 1)
+
+        if (boardArray[bombR][bombC] === 0 &&
+            bombR !== safeR &&
+            bombC !== safeC
+            ) {
+
             boardArray[bombR][bombC] = 'B'
-            cells[bombR * sizeBoard + bombC].classList.add('mine')
             aroundMine(bombR, bombC)
-        } else {
-            m--
+            mines--
         }
     }
 }
@@ -68,12 +107,10 @@ function aroundMine(mineR, mineC) {
         for (let c = -1; c <= 1; c++) {
             let newR = mineR + r
             let newC = mineC + c
-            if (newR >= 0 && newR < sizeBoard) {
-                if (newC >= 0 && newC < sizeBoard) {
+            if (newR >= 0 && newR < heightBoard) {
+                if (newC >= 0 && newC < widthBoard) {
                     if (boardArray[newR][newC] !== 'B') {
-                        boardArray[newR][newC] = boardArray[newR][newC] + 1
-                        cells[newR * sizeBoard + newC].classList.add('num')
-                        cells[newR * sizeBoard + newC].textContent = boardArray[newR][newC]
+                        boardArray[newR][newC]++
                     }
                 }
             }
@@ -81,39 +118,62 @@ function aroundMine(mineR, mineC) {
     }
 }
 
-function revealCell(cellId) {
-    let numId = Number(cellId)
-    let revealC = numId % sizeBoard
-    let revealR = (numId - revealC) / sizeBoard
+function revealCell(revealR, revealC) {
+    revealC = Number(revealC)
+    revealR = Number(revealR)
+
+    let numId = revealR * heightBoard + revealC
+
     if (cells[numId].classList.contains('flag')) return
-    else if (boardArray[revealR][revealC] == 'B') {
-        youLoose()
+    if (cells[numId].classList.contains('reveal')) return
+
+    if (boardArray[revealR][revealC] == 'B') {
+        cells[numId].classList.add('reveal')
+        cells[numId].querySelector('.cell-back').classList.add('mine')
+
+        if (!isLocked) youLoose()
+
         return
     }
+
+    if (boardArray[revealR][revealC] !== 0) {
+        cells[numId].classList.add('reveal')
+        revealed++
+
+        cells[numId].dataset['num'] = boardArray[revealR][revealC]
+        cells[numId].querySelector('.cell-back').textContent = boardArray[revealR][revealC]
+
+        return
+    }
+
+    cells[numId].classList.add('reveal')
+    revealed++
+
     for (let r = -1; r <= 1; r++) {
         for (let c = -1; c <= 1; c++) {
             let newR = revealR + r
             let newC = revealC + c
-            if (newR >= 0 && newR < sizeBoard) {
-                if (newC >= 0 && newC < sizeBoard) {
-                    if (!cells[newR * sizeBoard + newC].classList.contains('reveal') && boardArray[newR][newC] !== 'B') {
-                        cells[newR * sizeBoard + newC].classList.add('reveal')
-                        revealed++
-                        if (boardArray[newR][newC] == 0) {
-                            revealCell(cells[newR * sizeBoard + newC].dataset.cellId)
+            let newId = newR * heightBoard + newC
+
+            if (newR >= 0 && newR < heightBoard) {
+                if (newC >= 0 && newC < widthBoard) {
+                    if (!cells[newId].classList.contains('reveal') && boardArray[newR][newC] !== 'B') {
+
+                        if (boardArray[newR][newC] !== 'B') {
+                            setTimeout(() => {
+                                const [row, column] = Object.values(cells[newId].dataset).map(value => Number(value))
+                                revealCell(row, column)
+                            }, 50);
                         }
                     }
                 }
             }
         }
     }
-    if (revealed == sizeBoard ** 2 - numMines){
-        youWin()
-    }
 }
 
 function random(max) {
-    return Math.round(Math.random() * (max - 1))
+    return Math.floor(Math.random() * (max + 1))
 }
 
 function reset() {
@@ -121,14 +181,19 @@ function reset() {
 }
 
 function youLoose() {
-    cells.forEach(cell => {
-        if (!cell.classList.contains('flag')) {
-            cell.classList.add('reveal')
+    isLocked = true
+
+    for (r in boardArray) {
+        for (c in boardArray[r]) {
+            if (boardArray[r][c] === 'B') {
+                revealCell(r, c)
+            }
         }
-    });
-    setTimeout(() => {
-        alert('Perdeu otário hahahahha')
-    }, 1);
+    }
+
+    // setTimeout(() => {
+    //     alert('Perdeu otário hahahahha')
+    // }, 1);
 }
 
 function youWin() {
